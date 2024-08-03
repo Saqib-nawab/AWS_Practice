3a84a69372474bdf59fb6c0b9d4f520dbfbe9632
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2024-08-03 16:21:37 +0500

    hide aws security keys

--- aws-sdk-s3/index.js
+++ aws-sdk-s3/index.js
@@ -1,17 +1,17 @@
+require('dotenv').config();
 const { GetObjectCommand, S3Client } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
-//this is the client who is requesting something from bucket
+// this is the client who is requesting something from bucket
 const s3Client = new S3Client({
-    region: 'us-east-1',
-    Credential: {
-        accessKeyId: "AKIAZI2LIRHH53D5O4WX",
-        secretAccessKey: "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"
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     }
 });
 
-
-//command which is being requested
+// command which is being requested
 async function getObjectURL(key) {
     const command = new GetObjectCommand({
         Bucket: 'saqib-private',
@@ -21,8 +21,9 @@ async function getObjectURL(key) {
     const url = await getSignedUrl(s3Client, command);
     return url;
 }
+
 async function init() {
-    console.log('Image URL is: ', await getObjectURL('SaqibNawab.jpeg '));
+    console.log('Image URL is:', await getObjectURL('SaqibNawab.jpeg'));
 }
 
 init();
