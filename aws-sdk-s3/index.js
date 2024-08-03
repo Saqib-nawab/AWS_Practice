16bf7c5bf2d6e752eabc612be997a15d6e7887d8
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2024-08-03 16:59:49 +0500

    putobject and listobject commands in s3

--- aws-sdk-s3/index.js
+++ aws-sdk-s3/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { GetObjectCommand, S3Client } = require("@aws-sdk/client-s3");
+const { GetObjectCommand, S3Client, PutObjectCommand, ListObjectsV2Command } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 // this is the client who is requesting something from bucket
@@ -11,6 +11,7 @@ const s3Client = new S3Client({
     }
 });
 
+//how presigned URL works with Get objects
 // command which is being requested
 async function getObjectURL(key) {
     const command = new GetObjectCommand({
@@ -18,12 +19,46 @@ async function getObjectURL(key) {
         Key: key
     });
 
-    const url = await getSignedUrl(s3Client, command);
+    const url = await getSignedUrl(s3Client, command, { expiresIn: 20 });
     return url;
 }
 
+
+async function putObject(filename, contentType) { //user will provide file and contentType while uploading
+
+    const command = new PutObjectCommand({
+        Bucket: 'saqib-private',
+        Key: `/uploads/user-uploads/${filename}`,
+        ContentType: contentType
+    });
+
+    const url = await getSignedUrl(s3Client, command, { expiresIn: 20 });
+    return url;
+}
+
+// New function to list objects in the S3 bucket
+async function listObjects() {
+    const command = new ListObjectsV2Command({
+        Bucket: 'saqib-private'
+    });
+
+    try {
+        const response = await s3Client.send(command);
+        return response.Contents.map(object => object.Key);
+    } catch (error) {
+        console.error('Error listing objects:', error);
+        throw error;
+    }
+}
 async function init() {
     console.log('Image URL is:', await getObjectURL('SaqibNawab.jpeg'));
+
+    console.log('File upload URL is:', await putObject('test.jpg', 'image/jpeg'));
+
+
+    const objectKeys = await listObjects();
+    console.log('Objects in the bucket:', objectKeys);
 }
+
 
 init();
